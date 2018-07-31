7d39402608d80641499307f886c752d2b6ecf225
Yuta Yamaguchi <yuta.yamaguchi@gmail.com>
2018-07-30 21:35:56 -0500

    progress

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
-# PyBank Pseudo Code
-
-# Import OS and CSV Tools
 import os
 import csv
 
-# Open File Path
-filepath = os.path.join("..", "Resources", "budget_data.csv")
+filepath = os.path.join("Resources", "budget_data.csv")
 
-# Import CSV
-with open(filepath, "r", newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    for row in csvreader:
-        print(row)
+d = {}
+change = {}
+past_month = 0
+cur_month = 0
+diff = 0
+total = 0
 
-#skip the headers
-next(csvreader, None)
-
-# print the title
 print("Financial Analysis")
 print("-----------------------")
 
-## TOTAL MONTHS:
-    
-    # create a dictionary
-    dict = {}
-    
-    # loop through the csv and create a dictionary for each row
+with open(filepath, "r", newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
     for row in csvreader:
-        dict.update({'row[0]': row[1]})
-    
-    # for the total, count the number of entries in the dictionary
-    print("Total Months: " + len(dict))
+        d.update({row[0]:int(row[1])})
+        cur_month = int(row[1])
+        if past_month == 0:
+            change.update({row[0]:int(0)})
+        else:
+            diff = cur_month - past_month
+            change.update({row[0]:diff})
+        past_month = int(row[1])
+        
+print("Total Months: " + str(len(d)))
 
-## TOTAL
+with open(filepath, "r", newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
+    for row in csvreader:
+        total += int(row[1])
+        
+print("Total: $" + str(total))
+
+avg_total = 0
+for key, value in change.items():
+    avg_total += value
+    
+print("Average Change: $" + str(round(avg_total/85, 2)))
+
+maxvalue = max(change.values())
+for m in change.keys(): 
+    if change[m] == maxvalue:
+        print ("Greatest Increase in Profits: " + m + " ($" + str(change[m]) +")")
+
+
+minvalue = min(change.values())
+for k in change.keys(): 
+    if change[k] == minvalue:
+        print ("Greatest Decrease in Profits: " + k + " ($" + str(change[k]) +")")
